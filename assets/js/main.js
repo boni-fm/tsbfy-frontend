9cf40e9c4e44cb9df0ccd4391d97521c0e54fdce
Bonifasius Mamerutama <55308303+boni-fm@users.noreply.github.com>
2022-06-08 01:42:03 +0700

    about us and index

--- assets/js/main.js
+++ assets/js/main.js
@@ -243,3 +243,33 @@
   $(document).ready(function () {
     $('#example').DataTable();
   });
+
+  
+
+  /**
+   * Testimonials slider
+   */
+   new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 40
+      },
+
+      1200: {
+        slidesPerView: 3,
+      }
+    }
+  });
